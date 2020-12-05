c9641c63d69d5c6a3e82cb2118566b63e1395c65
ghiocgeorge <ghiocgeorge@gmail.com>
2020-12-05 09:46:54 +0200

    First commit

---
+++ src/main/java/ro/vladutit/Crypto/investments/CryptoInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package ro.vladutit.Crypto.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoInvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoInvestmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/vladutit/Crypto/investments/CryptoInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.vladutit.Crypto.investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoInvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
